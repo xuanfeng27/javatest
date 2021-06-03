8e1127a8f6b8b0f0c4f66b1a7ab628e7b3a4771d
zhaoll <82825287@qq.com>
2021-06-03 21:29:48 +0800

    '提交'

--- src/com/test/HelloWord.java
+++ src/com/test/HelloWord.java
@@ -1,19 +1,52 @@
 package com.test;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class HelloWord {
     int a = 4;
     int b = 9;
     public static void main(String[] args){
         System.out.println("Hello World");
-        for (String arg : args) {
-            System.out.println(arg);
-        }
+
+       // System.out.println(3480/1000*1000);//3000
+
+        // for (String arg : args) {
+        //    System.out.println(arg);
+      //  }
         new HelloWord().hello();
     }
     void hello(){
-        int c = a+b;
-        System.out.println(c);
+        String str = "[{'id':1,'name':'香蕉','price':8.0,'danwei':'公斤','quality':'A','number':5},{'id':2,'name':'苹果','price':6.5,'danwei':'公斤','quality':'B','number':3},{'id':3,'name':'橙子','price':9.0,'danwei':'公斤','quality':'A','number':4}]";
+        double total = 0.0;
+        JSONArray json = JSONArray.parseArray(str); // 首先把字符串转成 JSONArray  对象
+
+        if(json.size()>0){
+            for(int i=0;i<json.size();i++){
+                JSONObject job = json.getJSONObject(i);  // 遍历 jsonarray 数组，把每一个对象转成 json 对象
+                //System.out.println(job.get("id")) ;  // 得到 每个对象中的属性值
+                Double price = Double.parseDouble( job.get("price").toString());
+                Integer number = Integer.parseInt(job.get("number").toString()) ;
+                double sum = price*number;
+                total +=sum;
+                String aa = job.get("id").toString() +"\t"+ job.get("name").toString()+"\t"+
+                        job.get("price").toString()+"\t"+ job.get("danwei").toString()+"\t"+
+                        job.get("quality").toString()+"\t"+ job.get("number").toString()+"\t"+ sum;
+                System.out.println(aa) ;
+            }
+            System.out.println("一共是"+total) ;
+        }
+
+
+      //  int c = a+b;
+      //  System.out.println(c);
     }
+
+
+
 }
 
 /*
--- src/com/test/JavaseDay10TestHashMap.java
+++ src/com/test/JavaseDay10TestHashMap.java
@@ -17,6 +17,11 @@ public class JavaseDay10TestHashMap {
 
         int[] arr =new int[] {1,3,4,2,8,5};
         ArrayUtil.reverseArray(arr);
+
+        int[] intArr = new int[]{2,2,3,3,1,3,1,6};
+        int relt = new Solution().singleNumber(intArr);
+        System.out.println(Arrays.toString(intArr));
+        System.out.println("数组中不重复的元素是："+relt);
     }
 }
 
@@ -54,5 +59,34 @@ class HashMapTest {
         while (itr.hasNext()) {
             System.out.println(itr.next());
         }
+    }
+}
+
+
+
+//int [2,2,3,3,1,3,1,6]
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        /*int count =0;
+        for(int i :nums){
+            if(map.get(i)==null){
+                count = 1;
+            }else {
+                count+=1;
+            }
+            map.put(i,count);
+        }*/
+        for(int num:nums){
+            Integer count=map.get(num);
+           count = count==null?1:++count;
+            map.put(num,count);
+        }
+        for(int i:map.keySet()){
+            if(map.get(i)==1){
+                return i;
+            }
+        }
+        return -1;
     }
 }
